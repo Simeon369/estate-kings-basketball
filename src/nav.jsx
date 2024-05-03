56e26aaa5b1c115a25e8d8f735637642a061e867
Simeon <simeonogunyinka@gmail.com>
2024-05-03 19:31:29 +0100

    Second commit

--- src/nav.jsx
+++ src/nav.jsx
@@ -4,8 +4,8 @@ export default function Nav() {
     return(
         <nav>
 
-        <a href=""><img src="./public/images/kings-logo.png" alt="" className="logo" /></a>
-        <a href=""><img src="./public/images/menu-outline.svg" alt=""  className="logo" id="menu"/></a>
+        <a href=""><img src="./assets/images/kings-logo.png" alt="" className="logo" /></a>
+        <a href=""><img src="./assets/images/menu-outline.svg" alt=""  className="logo" id="menu"/></a>
         
         </nav>
     )
