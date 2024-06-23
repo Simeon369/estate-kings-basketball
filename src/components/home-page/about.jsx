8760c747fe8ebdf675a7abf97c93bf535b4d3103
Simeon <simeonogunyinka@gmail.com>
2024-06-23 18:10:15 +0100

    height adjustment

--- src/components/home-page/about.jsx
+++ src/components/home-page/about.jsx
@@ -3,9 +3,9 @@ import image from '../../assets/images/img1.jpg'
 
 function about() {
   return (
-    <div className='w-full h-screen bg-white flex md:flex-col items-center px-20 md:px-0 md:py-10'>
+    <div className='w-full bg-white flex md:flex-col items-center p-20 md:px-0 md:py-10'>
       <img src={image} alt=""  className='w-1/2 md:w-11/12 border-8 md:border-4 border-black rounded-3xl'/>
-      <div className='p-10 w-1/2 md:w-11/12 flex flex-col items-start md:items-center md:text-center'>
+      <div className='p-10 md:p-5 w-1/2 md:w-11/12 flex flex-col items-start md:items-center md:text-center'>
         <h1 className='font-bold text-4xl md:text-3xl'>Who Are We ?</h1>
         <p className='mt-4 text-slate-700 md:text-xs'>
         Welcome to Estate Kings Basketball Academy, where passion meets precision.
