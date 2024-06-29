060d3aa2ebfe0b985ea8c015a52c773b246b2ee6
Simeon <simeonogunyinka@gmail.com>
2024-06-29 19:23:30 +0100

    hero text margin top

--- src/components/home-page/hero.jsx
+++ src/components/home-page/hero.jsx
@@ -8,7 +8,7 @@ function hero() {
     <div className='w-full h-screen flex justify-center items-center'>
       <div className='hero fixed w-full h-screen bg-cover bg-center top-0 left-0 -z-20'></div>
       <Header />
-      <div className='flex flex-col items-center mt-24'>
+      <div className='flex flex-col items-center'>
         <h1 className='text-white sm:text-5xl text-8xl  text-center font-extrabold Shadow px-10 '>AMONGST MEN!! <br /> WE ARE <span className='text-Basketball'>KINGS</span></h1>
         <img src={logo} alt="" className='w-24 md:w-16'/>
         <button className=' mt-5 button'>Join the Kingdom</button>
